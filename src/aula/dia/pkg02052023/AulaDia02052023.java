a99c60ed137e662ec43eda73ae7222f79e9e839c
diego.rsilva52 <diego.rsilva52@CAS468306D160D.senacsp.edu.br>
2023-05-02 13:30:12 -0300

    First commit!

---
+++ src/aula/dia/pkg02052023/AulaDia02052023.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula.dia.pkg02052023;
+
+/**
+ *
+ * @author diego.rsilva52
+ */
+public class AulaDia02052023 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
