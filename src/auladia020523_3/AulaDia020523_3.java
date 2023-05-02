2e20f5c7546a86f049fe7463aa8d44f72621ede9
diego.rsilva52 <diego.rsilva52@CAS468306D160D.senacsp.edu.br>
2023-05-02 16:34:52 -0300

    2 exercicio

---
+++ src/auladia020523_3/AulaDia020523_3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package auladia020523_3;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author diego.rsilva52
+ */
+public class AulaDia020523_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um numero"));
+     int numero2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite um numero"));
+     JOptionPane.showMessageDialog(null, numero + numero2);
+     
+    }
+    
+}
